d81dd201bf1b619671c05b9ff1223e30b1018a35
inesfraccalvierii <ines.fraccalvieri@studio.unibo.it>
2025-03-03 11:02:00 +0100

    Initial refactor

---
+++ src/main/java/e1/SilverBankAccount.java
@@ -0,0 +1,25 @@
+package e1;
+
+public class SilverBankAccount implements BankAccount{
+
+    private CoreBankAccount base;
+
+    public SilverBankAccount() {
+        this.base =  new CoreBankAccount();
+    }
+
+    public int getBalance() {
+        return base.getBalance();
+    }
+
+    public void deposit(int amount) {
+        base.deposit(amount);
+    }
+
+    public void withdraw(int amount) {
+        if (this.getBalance() < amount){
+            throw new IllegalStateException();
+        }
+        base.withdraw(amount + 1);
+    }
+}
